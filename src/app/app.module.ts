b145f0fb778b5771014cf44e6fc7147818bef66a
Parzival516 <arnabsanyal228@gmail.com>
2020-10-16 18:32:07 +0530

    Initial Heroku commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { StudentsComponent } from './students/students.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    CoursesComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
